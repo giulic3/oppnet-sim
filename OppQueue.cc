3123adb70f9a9ea8ee58737e07730574edec1a00
Giulia Cantini <vermouth_95@hotmail.it>
2018-03-24 12:54:03 +0100

    add all parameters in .ini

--- OppQueue.cc
+++ OppQueue.cc
@@ -16,9 +16,6 @@
 #include "OppQueue.h"
 #include "Job.h"
 
-
-//using namespace omnetpp;
-
 namespace queueing {
 
 Define_Module(OppQueue);
@@ -152,76 +149,50 @@ void OppQueue::finish() {
 
 }; // namespace
 
-/*
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-//
-#include "OppQueue.h"
-Define_Module(OppQueue);
-void OppQueue::initialize() {
-    Queue::initialize();
-    // given that RNG is deterministic, all parameter sequences are the same (if the seeds are too)
-    visitTime = par("visitTime");
-    switchOverTime = par("switchOverTime");
-    serverIsAvailable = false;
-    serverIsIdle = true;
-    // when simulation starts, Q2 @ L1
-    isQ2LastLocation = false; // TODO: set true for L1 in .ned file
-    scheduleAt(simTime()+visitTime, startSwitchEvent);
-}
-void OppQueue::handleMessage(cMessage *msg) {
-    // self-messages
-    if (msg == startSwitchEvent) {
-        // TODO if server is processing a job now, the process must be interrupted
-        // check if server is idle
-        if (serverIsIdle == false) {
-            //...
-        }
-        serverIsAvailable = false;
-        // start switchOverTime timer
-        scheduleAt(simTime()+switchOverTime, endSwitchOverTimeEvent);
-    }
-    else if (msg == endSwitchOverTimeEvent) { // TODO refactor code in elegant way
-        if (isQ2LastLocation == true) {
-            // if true, Q2 was here and now's gone
-            // so server here stays unavailable
-        }
-        else { // Q2 was not here, and now it is at the end of the switch
-            serverIsAvailable = true;
-        }
-        scheduleAt(simTime()+visitTime, startSwitchEvent);
-    }
-    // messages from Q1/S1
-    else {
-        if (serverIsAvailable) {
-            serverIsIdle = false;
-            Queue::handleMessage(msg);
-            serverIsIdle = true; // TODO not sure
-        }
-        // server is not available
-        // TODO message is enqueued but will be processed
-        // only when the next handleMessage is called
-        else {
-            Job *job = check_and_cast<Job *>(msg);
-            arrival(job);//
-            this->queue.insert(job);
-            emit(queueLengthSignal, length());
-            job->setQueueCount(job->getQueueCount() + 1);
-            */
-       /* }
-    }
-}
-void OppQueue::refreshDisplay() const {
-}
- */
+
+/**
+ *
+ * void OppQueue::handleMessage(cMessage *msg) {
+-    // self-messages
+-    if (msg == startSwitchEvent) {
+-        // TODO if server is processing a job now, the process must be interrupted
+-        // check if server is idle
+-        if (serverIsIdle == false) {
+-            //...
+-        }
+-        serverIsAvailable = false;
+-        // start switchOverTime timer
+-        scheduleAt(simTime()+switchOverTime, endSwitchOverTimeEvent);
+-    }
+-    else if (msg == endSwitchOverTimeEvent) { // TODO refactor code in elegant way
+-        if (isQ2LastLocation == true) {
+-            // if true, Q2 was here and now's gone
+-            // so server here stays unavailable
+-        }
+-        else { // Q2 was not here, and now it is at the end of the switch
+-            serverIsAvailable = true;
+-        }
+-        scheduleAt(simTime()+visitTime, startSwitchEvent);
+-    }
+-    // messages from Q1/S1
+-    else {
+-        if (serverIsAvailable) {
+-            serverIsIdle = false;
+-            Queue::handleMessage(msg);
+-            serverIsIdle = true; // TODO not sure
+-        }
+-        // server is not available
+-        // TODO message is enqueued but will be processed
+-        // only when the next handleMessage is called
+-        else {
+-            Job *job = check_and_cast<Job *>(msg);
+-            arrival(job);//
+-            this->queue.insert(job);
+-            emit(queueLengthSignal, length());
+-            job->setQueueCount(job->getQueueCount() + 1);
+-            */
+-       /* }
+-    }
+-}
+ *
+ **/
