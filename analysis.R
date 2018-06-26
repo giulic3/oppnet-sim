7b08e936621c4fc439e6f46d6937651d5c818dbd
Giulia Cantini <giuliacantini3@gmail.com>
2018-06-26 19:34:17 +0200

    Implement batch means method in R

--- analysis.R
+++ analysis.R
@@ -1,35 +1,58 @@
 # batch analysis
 # first execute scavetool x *.sca -o oppnet-sim.csv or use omnet editor
 # TODO check which logs must be analyzed and automate the creation of .csv or use .json
+# perché ad ogni export ho sempre lo stesso numero di record?
 
 # change to the R script directory
 setwd("/home/giulia/git/oppnet-sim/")
 
-simdata <- read.csv("./results/oppnet-sim.csv", header=TRUE, sep=',')
-dim(simdata)
+# reads into a frame data?
+simData <- read.csv("./results/oppnet-sim.csv", header=TRUE, sep=',')
+dim(simData)
 
-batch <- function(x, nb, nobs) {
-  v <- rep(na,nb)
-  qq <- (x[1:nobs])
-  v[1] <- mean(qq)
-  
-  print(v[1])
-  
-  for (i in nb) {
-    a <- i*nobs+(i-1)
-    qq <- x[a:b]
-    v[i] <- mean(qq)
-    print(v[i])
-    
+# RemoveWarmUpPeriod or RemoveInitialTransient... to remove the first m data
+
+# x = vector containing the values
+# numBatches = number of batches
+# numObs = number of observations per batch
+BatchMeans <- function(x, numBatches, numObs) {
+
+  means <- rep(0, numBatches)
+  # observations to ignore
+  k <- 1000 #use RemoveWarmpUpPeriod
+
+  # repeat numBatches times
+  for (i in 1:numBatches) {
+
+    #for (j in numObs)
+    a <- i * numObs + 1 # not sure
+    b <- i * (numObs + 1) + 1 # not sure
+     # extract the vector portion that contains the batch
+    batch <- x[a:b]
+    means[i] <- mean(batch)
+    print(means[i])
+
+  #}
   }
-  
-  a1 <- mean(v)
-  b1 <- sd(v)
-  # b2
-  # c1 <- median
-  # d1 <- quantile(v, probs=0.25, nar.rm=TRUE)
-  # E1 <- quantile(v, probs=0.75, nar.rm=TRUE)
-  
-} 
+  finalMean <- mean(means)
+  standardDeviation <- sd(means)
+  # anche varianza?
+  #confidenceIntervalLeft <-
+  #confidenceIntervalRight <-
 
-batch(rstat,40,45)
+  
+
+  return c(finalMean, standardDeviation, confidenceIntervalLeft, confidenceIntervalRight)
+
+}
+
+results = BatchMeans(X, N, n)
+a <- r[1]
+b <- r[2]
+c <- r[3]
+d <- r[4]
+
+# batchMeans(totalServiceTimes,0,0)
+# batchMeans(numberOfJobs,0,0)
+# little's law...
+# ?
